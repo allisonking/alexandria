0a17de96eb7d821e769e5a5f312de5de47fc0597
Allison King <allisonjuliaking@gmail.com>
2020-05-02 10:13:12 -0400

    initial commit

---
+++ src/js/src/App.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import ThreePointVis from "./components/ThreePointVis/ThreePointVis";
+import "./styles.css";
+
+const data = new Array(1000).fill(0).map((d, id) => ({ id }));
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="vis-container">
+        <ThreePointVis data={data} />
+      </div>
+    </div>
+  );
+}
