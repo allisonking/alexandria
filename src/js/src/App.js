1fb7d0aaf0ea4d8fcd8431a1a09ef5c98cbdb811
Allison King <allisonjuliaking@gmail.com>
2020-05-02 10:30:26 -0400

    instance mesh

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import ThreePointVis from "./components/ThreePointVis/ThreePointVis";
 import "./styles.css";
 
-const data = new Array(1000).fill(0).map((d, id) => ({ id }));
+const data = new Array(100000).fill(0).map((d, id) => ({ id }));
 
 export default function App() {
   return (
